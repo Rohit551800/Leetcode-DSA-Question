ca33d36920050d84e3e593a8851b8e9d3d61c85e
Rohit551800 <rohitsingh139915@gmail.com>
2026-04-18 10:55:15 +0530

    Time: 2 ms (11.39%) | Memory: 45.2 MB (96.06%) - LeetSync

--- 3846-minimum-operations-to-make-array-sum-divisible-by-k/minimum-operations-to-make-array-sum-divisible-by-k.cpp
+++ 3846-minimum-operations-to-make-array-sum-divisible-by-k/minimum-operations-to-make-array-sum-divisible-by-k.cpp
@@ -5,7 +5,6 @@ public:
         for(int i=0;i<nums.size();i++){
             sum += nums[i];
         }
-        int ans = k - (sum % k);
-        return k - ans;
+        return sum % k;
     }
 };
