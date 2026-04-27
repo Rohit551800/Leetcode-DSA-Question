acdbcf79957ab6ec2ef40f5e6606681acf54343f
Rohit singh gouria <rohitsingh139915@gmail.com>
2026-04-27 13:36:47 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (54.23%) - LeetSync

--- 8-string-to-integer-atoi/string-to-integer-atoi.cpp
+++ 8-string-to-integer-atoi/string-to-integer-atoi.cpp
@@ -4,12 +4,12 @@ public:
         int n = s.size();
         long long ans = 0;
         int i = 0;
-        bool firstOp = false;
+        int firstOp = 1;
 
         while(i < n && s[i] == ' ') i++;
 
         if(i < n && (s[i] == '+' || s[i] == '-')){
-            if(s[i] == '-') firstOp = true;
+            if(s[i] == '-') firstOp = -1;
             i++;
         }
 
@@ -17,17 +17,16 @@ public:
         while(i < n){
             if(s[i] >= 48 && s[i] <= 57){
                 int x = s[i] - '0';
-
-                if(ans > (INT_MAX - x) / 10){
-                    return firstOp ? INT_MIN : INT_MAX;
-                }
                 ans = 10 * ans + x;
+
+                if( firstOp * ans >= INT_MAX) return INT_MAX;
+                if( firstOp * ans <= INT_MIN) return INT_MIN;
             }
             else{
                 break;
             }
             i++;
         }
-        return firstOp? -1 * ans : ans;
+        return (firstOp == -1 )? -1 * ans : ans;
     }
 };
