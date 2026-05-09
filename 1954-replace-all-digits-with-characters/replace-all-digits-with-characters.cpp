f46909f23d3edbe4054d458a8255ab719c912d21
Rohit singh gouria <rohitsingh139915@gmail.com>
2026-05-09 17:05:53 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (48.96%) - LeetSync

---
+++ 1954-replace-all-digits-with-characters/replace-all-digits-with-characters.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string replaceDigits(string s) {
+        int n = s.size();
+        string ans = "";
+        for(int i=0;i<n;i++){
+            if(isalpha(s[i])){
+                ans.push_back(s[i]);
+            }
+            else{
+                ans.push_back(ans[i-1] + s[i]-'0');
+            }
+        }
+        return ans;
+    }
+};
