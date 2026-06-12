d776bb012063448966c6ea093788df5a07d013b8
Rohit singh gouria <rohitsingh139915@gmail.com>
2026-06-13 01:18:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (84.12%) - LeetSync

---
+++ 3995-gcd-of-odd-and-even-sums/gcd-of-odd-and-even-sums.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int gcdOfOddEvenSums(int n) {
+        // int GCF = 0;
+        // int maxi = 0;
+        // for(int i = 1;i <= n;i++){
+        //     if(n%i == 0){
+        //         GCF = i;
+        //         maxi = max(maxi , i);
+        //     }
+        // }
+        // return maxi;
+
+        return n;
+    }
+};
