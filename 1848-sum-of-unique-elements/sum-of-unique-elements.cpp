11c0fa390596c5ad68aa1f39bb2b76f0e52df417
Rohit singh gouria <rohitsingh139915@gmail.com>
2026-06-18 03:26:24 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (94.60%) - LeetSync

--- 1848-sum-of-unique-elements/sum-of-unique-elements.cpp
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.cpp
@@ -2,14 +2,30 @@ class Solution {
 public:
     int sumOfUnique(vector<int>& nums) {
         //Bruteforce Solution
-        unordered_map<int , int>mpp;
-        for(auto it : nums){
-            mpp[it]++;
-        }
+        int n = nums.size();
         int sum = 0;
-        for(auto it : mpp){
-            if(it.second == 1) sum += it.first;
+        for(int i=0;i<n;i++){
+            bool dupli = false;
+            for(int j=0;j<n;j++){
+                if(i == j)continue;
+                if(nums[i] == nums[j]){
+                    dupli = true;
+                }
+            }
+            if(!dupli) sum += nums[i];
         }
         return sum;
+        //Better Solution Solution
+        // unordered_map<int , int>mpp;
+        // for(auto it : nums){
+        //     mpp[it]++;
+        // }
+        // int sum = 0;
+        // for(auto it : mpp){
+        //     if(it.second == 1) sum += it.first;
+        // }
+        // return sum;
+        // Tc = O(n) SC O(n)
+
     }
 };
