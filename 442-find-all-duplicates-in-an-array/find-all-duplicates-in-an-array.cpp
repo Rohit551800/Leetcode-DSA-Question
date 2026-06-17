89b644a40d7af59e77adf286e3bac64335a0ca83
Rohit singh gouria <rohitsingh139915@gmail.com>
2026-06-18 03:34:12 +0530

    Time: 52 ms (30.01%) | Memory: 67 MB (29.86%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
+        // vector<int>ans;
+        // int n = nums.size();
+        // unordered_map<int , int>mpp;
+        // for(auto it : nums){
+        //     mpp[it]++;
+        // }
+        // for(auto it : mpp){
+        //     if(it.second > 1)ans.push_back(it.first);
+        // }
+        // return ans;
+
         vector<int>ans;
-        int n = nums.size();
         unordered_map<int , int>mpp;
         for(auto it : nums){
-            mpp[it]++;
-        }
-        for(auto it : mpp){
-            if(it.second > 1)ans.push_back(it.first);
+            if(mpp.find(it) != mpp.end()){
+                ans.push_back(it);
+            }
+            else{
+                mpp[it] = 1;
+            }
         }
         return ans;
     }
