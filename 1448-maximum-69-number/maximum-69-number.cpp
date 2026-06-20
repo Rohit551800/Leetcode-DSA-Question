bc80e25c530d806525fde42a0c06adb2c9fc591c
Rohit singh gouria <rohitsingh139915@gmail.com>
2026-06-20 23:12:37 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (93.67%) - LeetSync

--- 1448-maximum-69-number/maximum-69-number.cpp
+++ 1448-maximum-69-number/maximum-69-number.cpp
@@ -10,20 +10,33 @@ public:
         return ans;
     }
     int maximum69Number (int num) {
-        vector<int>ans = findLast(num);
-        reverse(ans.begin() , ans.end());
+        // vector<int>ans = findLast(num);
+        // reverse(ans.begin() , ans.end());
 
-        int res = 0;
-        bool first = false;
-        for(auto it : ans){
-            if(it == 6 && first == false){
-                res = res*10 + 9;
-                first = true;
-            }
-            else{
-                res = res * 10 + it;
+        // int res = 0;
+        // bool first = false;
+        // for(auto it : ans){
+        //     if(it == 6 && first == false){
+        //         res = res*10 + 9;
+        //         first = true;
+        //     }
+        //     else{
+        //         res = res * 10 + it;
+        //     }
+        // }
+        // return res;
+
+
+        //Method second
+
+        string s = to_string(num);
+        for(int i=0;i<s.size();i++){
+            if(s[i] == '6'){
+                s[i] = '9';
+                break;
             }
         }
-        return res;
+        int ans = stoi(s);
+        return ans;
     }
 };
