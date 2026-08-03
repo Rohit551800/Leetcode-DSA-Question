01599eebd5513b76eecae4c0f8a8ae25c01c39a6
Rohit singh gouria <rohitsingh139915@gmail.com>
2026-08-04 01:55:45 +0530

    Time: 0 ms (100.00%) | Memory: 10.4 MB (97.78%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    int canWeSplit(vector<int>&nums , int m){
+        int n = nums.size();
+        int countK = 1 , prevSum = 0;
+        for(int i=0;i<n;i++){
+            if(prevSum + nums[i] <= m){
+                prevSum += nums[i];
+            }
+            else{
+                countK++;
+                prevSum = nums[i];
+            }
+        }
+        return countK;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        int n = nums.size();
+        // if(n < k) return -1;
+        int low = *max_element(nums.begin() , nums.end());
+        int high = accumulate(nums.begin() , nums.end() , 0);
+
+
+        //Bruteforce Solution
+
+        // for(int i=low;i<=high;i++){
+        //     int count = canWeSplit(nums , i);
+        //     if(count <= k){
+        //         return i;
+        //     }
+        // }
+
+        //Optimal Solution
+
+        while(low <= high){
+            int mid = low + high >> 1;
+            int count = canWeSplit(nums , mid);
+            if(count <= k) high = mid - 1;
+            else low = mid + 1;
+        }
+        return low;
+    }
+};
