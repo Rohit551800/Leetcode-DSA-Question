fa5ddd70649528306fbfb6e5a58beffb4f708ec8
Rohit singh gouria <rohitsingh139915@gmail.com>
2026-08-26 21:29:13 +0530

    Time: 4 ms (15.32%) | Memory: 16.4 MB (42.86%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -3,6 +3,7 @@ public:
     int f(vector<vector<int>>&grid , int i , int j , vector<vector<int>>&dp){
         if(i == 0 && j == 0) return grid[0][0];
         if(i < 0 || j < 0) return INT_MAX;
+
         if(dp[i][j] != -1) return dp[i][j];
         int left = f(grid , i , j-1 , dp);
         int up = f(grid , i-1 , j , dp);
@@ -10,9 +11,26 @@ public:
         return dp[i][j] = grid[i][j] + min(left , up);
     }
     int minPathSum(vector<vector<int>>& grid) {
-        int rows = grid.size();
-        int cols = grid[0].size();
-        vector<vector<int>> dp (rows , vector<int>(cols , -1));
-        return f(grid , rows-1 , cols-1 , dp);
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp (m , vector<int>(n , -1));
+        // return f(grid , m-1 , n-1 , dp); // Recurrence + Memoization
+
+        //Tabulation
+
+        dp[0][0] = grid[0][0];
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i == 0 && j == 0) continue;
+
+                int up = INT_MAX, left = INT_MAX;
+
+                if(i > 0) up = dp[i-1][j];
+                if(j > 0) left = dp[i][j-1];
+
+                dp[i][j] = grid[i][j]  + min(left , up);
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
