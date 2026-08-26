eb5edc79986b3f05a18e53da443495ccf027a21d
Rohit singh gouria <rohitsingh139915@gmail.com>
2026-08-26 21:50:18 +0530

    Time: 0 ms (100.00%) | Memory: 16.1 MB (74.39%) - LeetSync

--- 64-minimum-path-sum/minimum-path-sum.cpp
+++ 64-minimum-path-sum/minimum-path-sum.cpp
@@ -13,24 +13,52 @@ public:
     int minPathSum(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        vector<vector<int>> dp (m , vector<int>(n , -1));
+        // vector<vector<int>> dp (m , vector<int>(n , -1));
         // return f(grid , m-1 , n-1 , dp); // Recurrence + Memoization
 
         //Tabulation
 
-        dp[0][0] = grid[0][0];
+        // dp[0][0] = grid[0][0];
+        // for(int i=0;i<m;i++){
+        //     for(int j=0;j<n;j++){
+        //         if(i == 0 && j == 0) continue;
+
+        //         int up = INT_MAX, left = INT_MAX;
+
+        //         if(i > 0) up = dp[i-1][j];
+        //         if(j > 0) left = dp[i][j-1];
+
+        //         dp[i][j] = grid[i][j]  + min(left , up);
+        //     }
+        // }
+        // return dp[m-1][n-1];
+
+
+        //Space Optimization
+
+        vector<int>prev (n , 0);
+
         for(int i=0;i<m;i++){
+            vector<int>curr(n , 0);
             for(int j=0;j<n;j++){
-                if(i == 0 && j == 0) continue;
+                if(i==0 && j==0){
+                    curr[j] = grid[0][0];
+                    continue;
+                }
 
-                int up = INT_MAX, left = INT_MAX;
+                int left = INT_MAX;
 
-                if(i > 0) up = dp[i-1][j];
-                if(j > 0) left = dp[i][j-1];
+                int up = INT_MAX;
+                
+                if(i > 0){
+                    up = prev[j];
+                }
+                if(j > 0) left = curr[j-1];
 
-                dp[i][j] = grid[i][j]  + min(left , up);
+                curr[j] = grid[i][j]  + min(up , left);
             }
+            prev = curr;
         }
-        return dp[m-1][n-1];
+        return prev[n-1];
     }
 };
