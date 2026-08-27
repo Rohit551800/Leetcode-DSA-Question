0cf7ef3432c7e1c8de07f110d95e2893c7c03c80
Rohit singh gouria <rohitsingh139915@gmail.com>
2026-08-27 14:38:09 +0530

    Time: 0 ms (100.00%) | Memory: 12.9 MB (69.31%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -17,24 +17,46 @@ public:
         // return f(tri , 0 , 0);
 
         //Apply Memoization
-        vector<vector<int>>dp(n , vector<int>(n , -1));
+        // vector<vector<int>>dp(n , vector<int>(n , -1));
         // return f(tri , 0 , 0 , dp); //TLE
 
         //Tabulation
 
+        // vector<vector<int>>dp(n , vector<int>(n , -1));
+        // for(int i=0;i<n;i++){
+        //     dp[n-1][i] = tri[n-1][i];
+        // }
+
+        // for(int i=n-2;i>=0;i--){
+        //     for(int j=i;j>=0;j--){
+        //         int down  = INT_MAX , rdown = INT_MAX;
+        //         down = dp[i+1][j];
+        //         rdown = dp[i+1][j+1];
+
+        //         dp[i][j] = tri[i][j] + min(down , rdown);
+        //     }
+        // }
+        // return dp[0][0];
+
+        //Space Optimization
+
+        vector<int>prev(n , -1);
         for(int i=0;i<n;i++){
-            dp[n-1][i] = tri[n-1][i];
+            prev[i] = tri[n-1][i];
         }
 
         for(int i=n-2;i>=0;i--){
+            vector<int>curr(n , 0);
             for(int j=i;j>=0;j--){
-                int down  = INT_MAX , rdown = INT_MAX;
-                down = dp[i+1][j];
-                rdown = dp[i+1][j+1];
+                int down = INT_MAX , rdown = INT_MAX;
+                down = prev[j];
+                rdown = prev[j+1];
 
-                dp[i][j] = tri[i][j] + min(down , rdown);
+                curr[j] = tri[i][j] + min(down , rdown);
             }
+
+            prev= curr;
         }
-        return dp[0][0];
+        return prev[0];
     }
 };
