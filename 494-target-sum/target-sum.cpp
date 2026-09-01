a67e138eb4a4bb2627b20978afcb8c79e06d7f31
Rohit singh gouria <rohitsingh139915@gmail.com>
2026-09-02 02:40:42 +0530

    Time: 131 ms (35.13%) | Memory: 53.3 MB (11.92%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int f(int ind , int T , vector<int>&nums){
+    int f(int ind , int T , vector<int>&nums , vector<unordered_map<int ,int>>&dp){
         //Base Case;
         if(ind == 0){
             int ways = 0;
@@ -9,14 +9,15 @@ public:
             return ways;
         }
         //Explore all paths
+        if(dp[ind].count(T)) return dp[ind][T];
+        int neg = f(ind - 1 , T - nums[ind] , nums , dp);
+        int pos = f(ind - 1 , T + nums[ind] , nums , dp);
 
-        int neg = f(ind - 1 , T - nums[ind] , nums);
-        int pos = f(ind - 1 , T + nums[ind] , nums);
-
-        return neg + pos;
+        return dp[ind][T] = neg + pos;
     }
     int findTargetSumWays(vector<int>& nums, int T) {
         int n = nums.size();
-        return f(n-1 , T , nums);
+        vector<unordered_map<int , int>>dp(n);
+        return f(n-1 , T , nums , dp);
     }
 };
