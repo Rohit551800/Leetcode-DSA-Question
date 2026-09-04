5846129e70b1211cc871a59d6c2f82d406dce24c
Rohit singh gouria <rohitsingh139915@gmail.com>
2026-09-04 20:58:06 +0530

    Time: 31 ms (53.45%) | Memory: 26.9 MB (72.61%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    const int mod = 1e9 + 7;
     int f(int i , int j , string &s , string &t , vector<vector<int>>&dp){
         //Base Case
         if(j < 0) return 1;
@@ -12,7 +13,22 @@ public:
     }
     int numDistinct(string s, string t) {
         int n = s.size() , m = t.size();
-        vector<vector<int>>dp(n , vector<int>(m , -1));
-        return f(n-1 , m-1 , s , t , dp);
+        //Recurrence + Memoization
+
+        // vector<vector<int>>dp(n , vector<int>(m , -1));
+        // return f(n-1 , m-1 , s , t , dp);
+
+        //Tabulation
+        vector<vector<int>>dp(n+1 , vector<int>(m+1 , 0));
+        for(int j=0;j<=n;j++){
+            dp[j][0] = 1;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1] == t[j-1]) dp[i][j] = dp[i-1][j-1]%mod + dp[i-1][j]%mod;
+                else dp[i][j] = dp[i-1][j];
+            }
+        }
+        return dp[n][m];
     }
 };
