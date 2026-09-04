c4fc55440db00a704a6ce22fea467cc83f2b3b91
Rohit singh gouria <rohitsingh139915@gmail.com>
2026-09-04 22:16:30 +0530

    Time: 9 ms (89.27%) | Memory: 8.9 MB (95.85%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.cpp
+++ 115-distinct-subsequences/distinct-subsequences.cpp
@@ -34,14 +34,13 @@ public:
         //Space Optimization
 
         vector<int>prev(m+1 , 0) , curr(m+1 , 0);
-        prev[0] = 1 , curr[0] = 1;
+        prev[0] = 1;
         for(int i=1;i<=n;i++){
-            for(int j=1;j<=m;j++){
-                if(s[i-1] == t[j-1]) curr[j] = prev[j-1]%mod + prev[j]%mod;
-                else curr[j] = prev[j];
+            for(int j=m;j>=1;j--){
+                if(s[i-1] == t[j-1]) prev[j] = prev[j-1]%mod + prev[j]%mod;
+                else prev[j] = prev[j];
             }
-            prev = curr;
         }
-        return curr[m];
+        return prev[m];
     }
 };
