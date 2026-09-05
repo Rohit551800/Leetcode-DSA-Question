e0cd8c297f90423e105d0a2914940d55cbb37f01
Rohit singh gouria <rohitsingh139915@gmail.com>
2026-09-05 16:29:25 +0530

    Time: 7 ms (37.69%) | Memory: 24.6 MB (35.38%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.cpp
@@ -13,7 +13,26 @@ public:
         return dp[ind][buy] = profit;
     }
     int maxProfit(vector<int>& prices) {
-        vector<vector<int>>dp(prices.size() , vector<int>(2 , -1));
-        return f(0 , 1 , 0 ,prices , dp);
+        // Recursion + Memoization
+        // vector<vector<int>>dp(prices.size() , vector<int>(2 , -1));
+        // return f(0 , 1 , 0 ,prices , dp);
+
+        // Tabulation
+        int n = prices.size();
+        vector<vector<int>>dp(prices.size() + 1, vector<int>(2 , 0));
+        dp[n][0] =  dp[n][1] = 0;
+        for(int ind = n-1 ; ind>= 0;ind--){
+            for(int buy=0;buy<=1;buy++){
+                int profit = 0;
+                if(buy){
+                    profit = max(-prices[ind] + dp[ind + 1][0], 0 + dp[ind + 1][1]);
+                }
+                else{
+                    profit = max(prices[ind] + dp[ind + 1][1], 0 + dp[ind + 1][0]);
+                }
+                dp[ind][buy] = profit;
+            }
+        }
+        return dp[0][1];
     }
 };
