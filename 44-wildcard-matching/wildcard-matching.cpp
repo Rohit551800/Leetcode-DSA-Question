ff1e6843848074098d930e44015a723bf217bc09
Rohit singh gouria <rohitsingh139915@gmail.com>
2026-09-05 11:01:20 +0530

    Time: 57 ms (42.70%) | Memory: 15.9 MB (50.85%) - LeetSync

--- 44-wildcard-matching/wildcard-matching.cpp
+++ 44-wildcard-matching/wildcard-matching.cpp
@@ -18,7 +18,31 @@ public:
     }
     bool isMatch(string s, string t) {
         int n = s.size() , m = t.size();
-        vector<vector<int>>dp(n , vector<int>(m , -1));
-        return f(n-1 , m-1 ,s , t , dp);
+        //Recursion + Memoization
+
+        // vector<vector<int>>dp(n , vector<int>(m , -1));
+        // return f(n-1 , m-1 ,s , t , dp);
+
+        //Tabulation
+        vector<vector<bool>>dp(n+1 , vector<bool>(m+1 , false));
+
+        //Base case
+        dp[0][0] = true;
+
+        for(int j = 1; j <= m; j++){
+            if(t[j-1] == '*'){
+                dp[0][j] = dp[0][j-1];
+            }
+        }
+        // Explore all the paths
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1] == t[j-1] || t[j-1] == '?') dp[i][j] = dp[i-1][j-1];
+                else if(t[j-1] == '*')dp[i][j] = dp[i][j-1]|| dp[i-1][j] ;
+                else dp[i][j] = false;
+            }
+        }
+        return dp[n][m];
     }
 };
