70d2a63c1c0bb1dd153fedcb0524e264f905c382
Rohit singh gouria <rohitsingh139915@gmail.com>
2026-09-26 13:27:09 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (29.78%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.cpp
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -14,8 +14,8 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         //Apply Memoization
-        // vector<vector<int>>dp(m , vector<int>(n , -1));
-        // return findPaths(grid , m-1 , n-1 , dp); // Memoization
+        vector<vector<int>>dp(m , vector<int>(n , -1));
+        return findPaths(grid , m-1 , n-1 , dp); // Memoization
 
 
         //Tabulation
@@ -40,27 +40,27 @@ public:
 
         // Space Optimization
 
-        vector<int>prev(n , 0);
+        // vector<int>prev(n , 0);
 
-        for(int i=0;i<m;i++){
-            vector<int>curr(n , 0);
-            for(int j=0;j<n;j++){
-                if((i == 0 && j == 0) && grid[i][j] != 1){ 
-                    curr[j] = 1;
-                    continue;
-                }
-                if(grid[i][j] == 1){ 
-                    curr[j] = 0;
-                    continue;
-                }
-                int left = 0, up = 0;
-                if(i > 0) up = prev[j];
-                if(j > 0) left = curr[j-1];
+        // for(int i=0;i<m;i++){
+        //     vector<int>curr(n , 0);
+        //     for(int j=0;j<n;j++){
+        //         if((i == 0 && j == 0) && grid[i][j] != 1){ 
+        //             curr[j] = 1;
+        //             continue;
+        //         }
+        //         if(grid[i][j] == 1){ 
+        //             curr[j] = 0;
+        //             continue;
+        //         }
+        //         int left = 0, up = 0;
+        //         if(i > 0) up = prev[j];
+        //         if(j > 0) left = curr[j-1];
 
-                curr[j] = left + up;
-            }
-            prev = curr;
-        }
-        return prev[n-1];
+        //         curr[j] = left + up;
+        //     }
+        //     prev = curr;
+        // }
+        // return prev[n-1];
     }
 };
